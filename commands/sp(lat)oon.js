877e748ce6fce586613ba85b66bc4268a50fae6c
Omar <910212004abc@gmail.com>
2018-10-05 21:35:01 +1000

    2

---
+++ commands/sp(lat)oon.js
@@ -0,0 +1,10 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bots, message, args) => {
+    return message.channel.send("https://i.redd.it/xw9igs0hjhc11.jpg");
+}
+
+module.exports.help = {
+    name: "sp(lat)oon"
+}
--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 git add .
-set /a FILENAME=(%RANDOM%*var/32768+1)
-git commit -am "IDK %FILENAME%"
+set /a FILENAME=%RANDOM% * 100 / 32768 + 1
+git commit -am "%FILENAME%"
 git push -u origin master
 end
