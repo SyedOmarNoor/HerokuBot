d9eccc2f4f36de30d892270a441f6f4db9aa325e
Omar <910212004abc@gmail.com>
2018-10-06 11:38:42 +1000

    yayyay

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -9,6 +9,7 @@ module.exports.run = async (bots, message, args) => {
     if(!tomute) return message.reply("Couldn't find that user.");
     if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't mute them.");
     let muterole = message.guild.roles.find(`name`, "muted");
+    //start of create role
     if(!muterole){
         try{
             muterole = await message.guild.createRole({
