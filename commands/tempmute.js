98dd1491d9974e0956468c2623340f7c8c2a4f25
Omar <910212004abc@gmail.com>
2018-10-06 13:29:53 +1000

    7289

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -8,42 +8,46 @@ module.exports.run = async (bots, message, args) => {
     let tomute = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!tomute) return message.reply("Couldn't find that user.");
     if(!tomute.hasPermission("ADMINISTRATOR")) return message.reply("You can't mute them.");
+    
     let muterole = message.guild.roles.find(`name`, "muted");
     //start of create role
-    if(!muterole){
-        try{
-            muterole = await message.guild.createRole({
-                name: "muted",
-                color:"#000000",
-                permissions: []
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muterole, {
-                    SEND_MESSAGES: false,
-                    ADD_REACTIONS: false
-                
+    if(tomute.hasPermission("ADMINISTRATOR")){
+        
+        if(!muterole){
+            try{
+                muterole = await message.guild.createRole({
+                    name: "muted",
+                    color:"#000000",
+                    permissions: []
+                })
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(muterole, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false
+                    
+                    });
                 });
-            });
-        }catch(e){
-            console.log(e.stack)
+            }catch(e){
+                console.log(e.stack)
+            }
         }
+        //end of create role
+        let mutetime = args[1];
+        if(!mutetime) return message.reply("You didn't specify a time.");
+
+        await(tomute.addRole(muterole.id));
+        let tempmutechannel = message.guild.channels.find(`name`, "incidents");
+        if(!tempmutechannel) return message.channel.send("Couldn't find any 'incidents' channel.")
+        message.delete().catch(O_o=>{});
+        tempmutechannel.send(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}.`);
+
+        setTimeout(function(){
+            tomute.removeRole(muterole.id);
+            tempmutechannel.send(`<@${tomute.id}> has been unmuted!`);
+        }, ms(mutetime));
+
+
     }
-    //end of create role
-    let mutetime = args[1];
-    if(!mutetime) return message.reply("You didn't specify a time.");
-
-    await(tomute.addRole(muterole.id));
-    let tempmutechannel = message.guild.channels.find(`name`, "incidents");
-    if(!tempmutechannel) return message.channel.send("Couldn't find any 'incidents' channel.")
-    message.delete().catch(O_o=>{});
-    tempmutechannel.send(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}.`);
-
-    setTimeout(function(){
-        tomute.removeRole(muterole.id);
-        tempmutechannel.send(`<@${tomute.id}> has been unmuted!`);
-    }, ms(mutetime));
-
-
 //end of module
 }
 
