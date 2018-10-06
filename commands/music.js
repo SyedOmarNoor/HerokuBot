17c1b7fad4b7113e6e4f66ca8e0b5bcfa0eb014f
Omar <910212004abc@gmail.com>
2018-10-06 11:35:37 +1000

    456

--- commands/music.js
+++ commands/music.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bots, message, args) => {
-    
+    if(!args[1]) {
+        
+    }
 }
 
 module.exports.help = {
--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,6 +1,31 @@
 const Discord = require("discord.js");
+const ms = require("ms");
 
 module.exports.run = async (bots, message, args) => {
+
+    //!!tempmute @user 1s/m/h/d
+
+    let tomute = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!tomute) return message.reply("Couldn't find that user.");
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't mute them.");
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole){
+        try{
+            muterole = await message.guild.createRole({
+                name: "muted",
+                color:"#000000",
+                permissions: []
+            })
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermission(muterole, {
+                    SEND_MESSAGES: false
+                
+                });
+            });
+        }catch(e){
+            console.log(e.stack)
+        }
+    }
     
 }
 
