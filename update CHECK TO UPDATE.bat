f12fd7603b2d91f5204135425a4a313277c69a6e
Omar <910212004abc@gmail.com>
2018-10-07 12:29:04 +1100

    6169

--- update CHECK TO UPDATE.bat
+++ update CHECK TO UPDATE.bat
@@ -1,7 +1,7 @@
 set /a FILENAME=%RANDOM% * 10000 / 32768 + 1
 git add .
 git status | find "Your branch is ahead of 'origin/master'"
-if find "Your branch is ahead of 'origin/master'" = true (
+if find = true (
 git commit -am "%FILENAME%"
 git push -u origin master
 )
