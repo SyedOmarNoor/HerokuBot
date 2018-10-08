1461b7c65993f7936d3e57666f02138ed38bf609
Omar <910212004abc@gmail.com>
2018-10-08 16:43:24 +1100

    7481

--- commands/rules.js
+++ commands/rules.js
@@ -7,8 +7,8 @@ module.exports.run = async (bots, message, args) => {
     .setDescription("Rules")
     .setColor("#000000")
     .addField("Be a good person", "That includes:")
-    .addField("#1:", "Do not expose other people to inappropriate or undesired material (pictures, videos, audio) or behaviours (spamming, excessive use of swear and/or curse words).");
-
+    .addField("#1:", "Do not expose other people to inappropriate or undesired material (inappropriate pictures or videos, loud audio) or behaviours (spamming, 'Sucide' (spelling, Darren) encouragement or threats, racism, excessive use of swear and/or curse words).")
+    .addField("#2:", "");
     return message.channel.send(rulesEmbed);
 }
 
