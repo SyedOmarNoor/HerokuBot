426f165c81f59b5334006d11e853b60a692d386e
Omar <910212004abc@gmail.com>
2018-10-08 13:16:11 +1100

    5090

--- commands/rules.js
+++ commands/rules.js
@@ -7,6 +7,7 @@ module.exports.run = async (bots, message, args) => {
     .setDescription("Rules")
     .setColor("#000000")
     .addField("Be a good person, like (what) Omar (should be).")
+    .addBlankField("whats blank field")
     .addField("#1:", "Do not expose other people to inappropriate or undesired material (pictures, videos, audio, certain words used excessively).");
 
     return message.channel.send(rulesEmbed);
