d7f4daa6828cb2f7ed1c4bd1208557e6d2632c46
Omar <910212004abc@gmail.com>
2018-10-08 12:43:56 +1100

    3163

--- update (enter name, check to update).bat
+++ update (enter name, check to update).bat
@@ -1,9 +1,10 @@
 @echo off
 git add .
 git status | find "Your branch is ahead of 'origin/master'"
-set /p FILENAME="Enter the comment for your commit."
+set /p FILENAME = "Enter the comment for your commit."
 if errorlevel = 0 (
     echo name set.
+    set /p FILENAME = "Enter the comment for your commit."
     git commit -am "%FILENAME%"
     git push -u origin master
 )
