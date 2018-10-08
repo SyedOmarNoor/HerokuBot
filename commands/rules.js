8fdaa9037cb44c7cb5dee0e649fc234a02049e8a
Omar <910212004abc@gmail.com>
2018-10-08 13:14:13 +1100

    4974

--- commands/rules.js
+++ commands/rules.js
@@ -6,7 +6,7 @@ module.exports.run = async (bots, message, args) => {
     
     .setDescription("Rules")
     .setColor("#000000")
-    .addField(" ","Be a good person, like (what) Omar (should be)." )
+    .addField("Be a good person, like (what) Omar (should be)." )
     .addField("That includes:")
     .addField("#1:", "Do not expose other people to inappropriate or undesired material (pictures, videos, audio, certain words used excessively).");
 
