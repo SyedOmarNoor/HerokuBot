5977a615e13de95547b1ab2809f6ab04fcd1cc03
Omar <910212004abc@gmail.com>
2018-10-08 16:48:58 +1100

    7815

--- commands/rules.js
+++ commands/rules.js
@@ -8,7 +8,7 @@ module.exports.run = async (bots, message, args) => {
     .setColor("#000000")
     .addField("Be a good person", "That includes:")
     .addField("#1:", "Do not expose other people to inappropriate or undesired material (inappropriate pictures or videos, loud audio) or behaviours (spamming, 'Sucide' (spelling, Darren) encouragement or threats, racism, excessive use of swear and/or curse words).")
-    .addField("#2:", "");
+    .addField("#2:", "Use specific channels for specific purposes. If you want to say something random, you are more likely to be allowed to say it in #general instead of, for example, #announcements. If you are unsure of what a channel is for, direct message Darren Cherk (@Pandemic#9988 (Server Owner, Server Creator and Core)) for information.");
     return message.channel.send(rulesEmbed);
 }
 
