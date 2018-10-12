ea895ce04603b0cdce69a95a57cf659834a4b46e
Omar <910212004abc@gmail.com>
2018-10-12 22:18:22 +1100

    1926

---
+++ commands/clear.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bots, message, args) => {
+    //!!clear 15
+    if(!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("No.");
+    if(!args[0]) return message.channel.send("No amount stated");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send('Cleared ${args[0]} messages.').then(msg => msg.delete(5000));
+    });
+}
+
+module.exports.help = {
+    name: "clear"
+}
