a8ec8e44c3bf8586043c483e66e3121095282cc1
Omar <910212004abc@gmail.com>
2018-10-13 10:34:05 +1100

    5918

--- utils/errors.js
+++ utils/errors.js
@@ -7,6 +7,7 @@ module.exports.noPerms = (message, perm) => {
     let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setTitle("No Permissions")
+    .setColor("#ff0000")
     .addField("Insufficient permission/s", perm);
 
     message.channel.send(embed).then(m => m.delete(5000));
