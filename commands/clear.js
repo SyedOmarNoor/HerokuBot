431e2b7690804ce446ee6157a796a5584bb95875
Omar <910212004abc@gmail.com>
2018-10-13 14:44:21 +1100

    883

--- commands/clear.js
+++ commands/clear.js
@@ -5,6 +5,7 @@ module.exports.run = async (bots, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
     if(!args[0]) return message.channel.send("No amount stated");
     message.channel.bulkDelete(args[0]).then(() => {
+        message.delete().catch(O_o=>{});
         message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
     });
 }
