1f5a879b7ece69a9c08ef79d4d6a86c8a96d2315
Omar <910212004abc@gmail.com>
2018-10-16 12:43:07 +1100

    1945

--- commands/spoon.js
+++ commands/spoon.js
@@ -1,10 +1,18 @@
-
 const Discord = require("discord.js");
+const superagent = require("superagent");
 
 module.exports.run = async (bots, message, args) => {
-    return message.channel.send("https://i.redd.it/xw9igs0hjhc11.jpg");
+    let {body} = await superagent
+    .get('https://i.redd.it/xw9igs0hjhc11.jpg');
+
+    let spoonembed = new Discord.RichEmbed()
+    .setColor("#ffffff")
+    //.setTitle("Cat :cat:")
+    .setImage(body);
+
+    message.channel.send(spoonembed);
 }
 
 module.exports.help = {
-    name: "spoon"
+    name: "cat"
 }
