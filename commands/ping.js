7a49ea1f9e3db6e751cc39500c033122f4de4f27
Omar <910212004abc@gmail.com>
2018-10-16 17:06:33 +1100

    7699

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bots, message, args) => {
     message.delete().catch(O_o=>{});
-    message.channel.send("@everyone");
+    message.channel.send(`@everyone`);
 }
 
 module.exports.help = {
