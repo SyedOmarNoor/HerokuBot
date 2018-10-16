a1aca3ab92639fdb434e154e72381ecd48c7f26b
Omar <910212004abc@gmail.com>
2018-10-16 17:07:10 +1100

    7736

--- commands/say.js
+++ commands/say.js
@@ -6,7 +6,7 @@ module.exports.run = async (bots, message, args) => {
     //Hi!
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-    let botmessage = args.join(" ");
+    let botmessage = args.join(` `);
     message.delete().catch();
     message.channel.send(botmessage);
     
