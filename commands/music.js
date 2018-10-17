b96174db2f07b98f4337da7464f43fce29275d9f
Omar <910212004abc@gmail.com>
2018-10-17 16:59:13 +1100

    3365

--- commands/music.js
+++ commands/music.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const process.env.FFMPEG_PATH = './node_modules/ffmpeg-binaries/bin/ffmpeg.exe'
+//var ffmpegPath = './node_modules/ffmpeg-binaries/bin/ffmpeg.exe'
 
 module.exports.run = async (bots, message, args) => {
     if(!args[0]) {
@@ -12,7 +13,7 @@ module.exports.run = async (bots, message, args) => {
         message.channel.send("Please join a voice channel.");
         return;
     }
-    if (message.guild.me.voiceChannel) return message.channel.send('Sorry, the bot is already connected to a musc channel.');
+    //if (message.guild.me.voiceChannel) return message.channel.send('Sorry, the bot is already connected to a music channel.');
 
     let validate = await ytdl.validateURL(args[0]);
     if (!validate) return message.channel.send('Sorry, please input a valid url following the command.');
