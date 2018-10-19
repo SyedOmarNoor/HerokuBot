7c05360269043a535f084501ac6517e5483f1226
Omar <910212004abc@gmail.com>
2018-10-19 17:24:37 +1100

    7091

--- commands/spoon.js
+++ commands/spoon.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
-const superagent = require("superagent");
+let spoon = './xw9igs0hjhc11.jpg';
 
 module.exports.run = async (bots, message, args) => {
 
     let spoonembed = new Discord.RichEmbed()
     .setColor("#ffffff")
     .setTitle("Sp(lat)oon")
-    .setImage('./commands/xw9igs0hjhc11.jpg');
+    .setImage('./xw9igs0hjhc11.jpg');
 
     message.channel.send(spoonembed);
 }
