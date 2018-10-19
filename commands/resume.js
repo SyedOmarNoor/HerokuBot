27e08f156bc63e29a2a7c74e6c58051cd39cae2e
Omar <910212004abc@gmail.com>
2018-10-19 22:15:42 +1100

    4497

--- commands/resume.js
+++ commands/resume.js
@@ -1,6 +1,15 @@
-const Discord = require("discord.js");
 const queue = new Map();
-
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if(server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    })
+}
 module.exports.run = async (bots, message, args) => {
     const serverQueue = queue.get[message.guild.id];
     if (serverQueue && !serverQueue.playing) {
