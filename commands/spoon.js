45f31a2be710d1741d5144c0b64141878c3fbb61
Omar <910212004abc@gmail.com>
2018-10-19 18:20:43 +1100

    446

--- commands/spoon.js
+++ commands/spoon.js
@@ -4,9 +4,10 @@ let spoon = './xw9igs0hjhc11.jpg';
 module.exports.run = async (bots, message, args) => {
 
     let spoonembed = new Discord.RichEmbed()
-    .setColor("#ffffff")
+
     .setTitle("Sp(lat)oon")
-    .setImage('./xw9igs0hjhc11.jpg');
+    .setColor("#ffffff")
+    .setImage('..commands//xw9igs0hjhc11.jpg');
 
     message.channel.send(spoonembed);
 }
