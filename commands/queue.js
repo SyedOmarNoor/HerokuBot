6fcc2dae6908f32fa5754b562c309dfd6faf7f63
Omar <910212004abc@gmail.com>
2018-10-19 21:57:50 +1100

    3429

--- commands/queue.js
+++ commands/queue.js
@@ -4,10 +4,7 @@ const queue = new Map();
 module.exports.run = async (bots, message, args) => {
     const serverQueue = queue.get(message.guild.id);
     if (!serverQueue) return message.channel.send('There is nothing playing.');
-		return message.channel.send(`
-__**Song queue:**__
-${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
-**Now playing:** ${serverQueue.songs[0].title}
+		return message.channel.send(` __**Song queue:**__ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')} **Now playing:** ${serverQueue.songs[0].title}
 		`);
 }
 
