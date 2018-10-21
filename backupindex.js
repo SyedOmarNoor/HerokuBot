8ee0f1ea2db77c8d19c32ecd3d8b237951f9fe35
Omar <910212004abc@gmail.com>
2018-10-21 20:23:20 +1100

    9986

--- backupindex.js
+++ backupindex.js
@@ -29,7 +29,7 @@ fs.readdir("./commands", (err, files) => {
 bot.on("ready", async () => {
 console.log(`${bot.user.username} should be online. Can you go see if it is?`);
 //console.log(`Server : ${message.guild.name} | Channel : #${message.channel.name} | Userid : ${message.author.tag} | Message : ${message.content}\n`);
-bot.user.setActivity("people say !!help", {type: "WATCHING"});
+bot.user.setActivity("people not say !!help", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
