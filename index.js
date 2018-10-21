b30e340934b5ffc541a78c68890bb9d9069dbc28
Omar <910212004abc@gmail.com>
2018-10-21 17:09:12 +1100

    8377

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let args = messageArray.slice(1);
 
 var servers = {};
 
-if(cmd != `v!`) return;
+if(cmd === `v!`) return;
 
 let commandfile = bot.commands.get(cmd.slice(prefix.length));
 if(commandfile) commandfile.run(bot, message, args);
