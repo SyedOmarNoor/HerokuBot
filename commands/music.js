3140f9f20923cb3f4ff9982c54c036427f96ad39
Omar <910212004abc@gmail.com>
2018-10-30 17:15:07 +1100

    3669

--- commands/music.js
+++ commands/music.js
@@ -5,3 +5,12 @@
 // const youtube = new YouTube('');
 // const bot = new Discord.Client({disableEveryone: true});
 // let cmd2 = args[0]
+const Discord = require("discord.js");
+
+module.exports.run = async (bots, message, args) => {
+    // (code)
+}
+
+module.exports.help = {
+    name: "(name)"
+}
