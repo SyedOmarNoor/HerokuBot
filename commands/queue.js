01df4a81536b2395acbb61478e914d1128f32df0
Omar <910212004abc@gmail.com>
2018-10-30 17:17:07 +1100

    3788

--- commands/queue.js
+++ commands/queue.js
@@ -1,12 +1,12 @@
-import ('../index.js') ;
-const Discord = require("discord.js");
-var ffmpegPath = './node_modules/ffmpeg-binaries/bin/ffmpeg.exe';
+// import ('../index.js') ;
+// const Discord = require("discord.js");
+// var ffmpegPath = './node_modules/ffmpeg-binaries/bin/ffmpeg.exe';
 
-module.exports.run = async (bots, message, args) => {
-    var serverQueue = queue.get(message.guild.id);
-    if (!serverQueue) return message.channel.send('There is nothing playing.');
-		return message.channel.send(` __**Song queue:**__ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')} **Now playing:** ${serverQueue.songs[0].title}`);
-}
+// module.exports.run = async (bots, message, args) => {
+//     var serverQueue = queue.get(message.guild.id);
+//     if (!serverQueue) return message.channel.send('There is nothing playing.');
+// 		return message.channel.send(` __**Song queue:**__ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')} **Now playing:** ${serverQueue.songs[0].title}`);
+// }
 
 module.exports.help = {
     name: "queue"
