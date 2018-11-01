57bdb6d2bc975cc2d2ccfcdaa9b17e84281c2cca
Omar <910212004abc@gmail.com>
2018-11-01 18:37:22 +1100

    795

--- commands/yay.js
+++ commands/yay.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
+const yayify = require('yayify');
 
 module.exports.run = async (bots, message, args) => {
     //!!say Hi!
@@ -9,11 +10,11 @@ module.exports.run = async (bots, message, args) => {
     }
     //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
     let botmessage = args.join(" ");
+    let yay = yayify(botmessage); 
     message.delete().catch(O_o=>{});
-    message.channel.send(botmessage);
-    
+    message.channel.send(yay);
 }
 
 module.exports.help = {
-    name: "say"
+    name: "yay"
 }
