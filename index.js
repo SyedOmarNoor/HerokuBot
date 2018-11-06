e4267ad63b2153f9fc5ee1751f1c3d2a083a419c
Omar <910212004abc@gmail.com>
2018-11-06 12:38:25 +1100

    yay212121

--- index.js
+++ index.js
@@ -23,6 +23,23 @@ fs.readdir("./commands", (err, files) => {
     });
 
 });
+fs.readdir("./commands/music", (err, files) => {
+
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Couldn't find commands");
+        return;
+    }
+
+    jsfile.forEach((f, i) =>{
+        let props = require(`./commands/${f}`);
+        console.log(`${f}(music command) is present and loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+
+});
 
 // In UNIX double slash (//) is not used for commenting but a hashtag (#) is.
 
