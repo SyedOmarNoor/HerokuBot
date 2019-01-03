4a4932d711910f366a7eec4ae565c459e6ee8443
Omar <910212004abc@gmail.com>
2019-01-03 15:33:11 +1100

    4343

--- commands/maths.js
+++ commands/maths.js
@@ -1,11 +1,16 @@
 const Discord = require("discord.js");
+const math = require('mathjs');
 
 module.exports.run = async (bots, message, args) => {
     // let mathsyay = args.split();
 
     // let solution = eval(mathsyay);
-
-
+    let resp;
+    try {
+        resp = math.eval(args);
+    } catch (e) {
+        return message.channel.send("Input a valid equation")
+    }
     
 }
 
