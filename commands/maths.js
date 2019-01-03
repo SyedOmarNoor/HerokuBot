8d8243c66092f7855d5096f2f36bc7238e863c9f
Omar <910212004abc@gmail.com>
2019-01-03 15:57:46 +1100

    5813

--- commands/maths.js
+++ commands/maths.js
@@ -7,14 +7,11 @@ module.exports.run = async (bots, message, args) => {
     // let solution = eval(mathsyay);
     
     let resp;
-    let yayay = args.replace(/x/g, "*"); 
     try {
-        resp = math.eval(yayay);
+        resp = math.eval(args);
     } catch (e) {
         return message.channel.send("Input a valid equation");
-    };
-    
-    //resp.allReplace({'x': '*', 'b': 'o'})
+    }
     message.channel.send(resp);
 }
 
